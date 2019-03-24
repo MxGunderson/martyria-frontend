0f71432f6ad868efda70c91fef490d65f5dc6916
matthew.gunderson12@gmail.com <matthew.gunderson12@gmail.com>
2019-03-23 19:45:40 -0700

    added a 'TextBody' component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 import 'react-materialize';
 import './App.css';
 import Header from './Components/Header';
+import TextBody from './Components/TextBody';
+import Footer from './Components/Footer';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <TextBody />
+        <Footer />
       </div>
     );
   }
---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import 'react-materialize';
+
+
+function Footer() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Footer;
