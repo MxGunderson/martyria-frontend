abf16b32b594153e29df0ef32ad64f7e0982cf8e
matthew.gunderson12@gmail.com <matthew.gunderson12@gmail.com>
2019-03-26 21:31:10 -0700

    added an image to 'Home' page
    attemping to make image a backdrop to the home page.
    
    image location in Img/homeBanner.jpg
    
    Rendering from Img/HomeBanner.js to Home in 'pages/Home.js'

---
+++ src/Components/Img/HomeBanner.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import homeBanner from './homeBanner.jpg';
+
+export default () => {
+    return (
+        <div>
+            <img src={homeBanner} alt="Loading..." style={{width: '100%', margin: 'auto', display: 'block'}} />
+        </div>
+    )
+}
--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Col, Row } from 'react-materialize';
+import HomeBanner from '../Img/HomeBanner';
 
 class Home extends Component {
   render() {
@@ -8,7 +9,6 @@ class Home extends Component {
         < div className="container" >
           <div className="textBody">
             <Row>
-              <h1>Home Page!</h1>
               <Col s={6} className='grid-text'>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
@@ -31,6 +31,12 @@ class Home extends Component {
                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
               </Col>
+
+              {/* 'HomeBanner is an image' */}
+              <Col s={12} className='grid-text'>
+                <HomeBanner />
+              </Col>
+              
               <Col s={12} className='grid-text'>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
