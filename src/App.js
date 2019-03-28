df7e61e46c85911715f8b81be4e9f01385cd8a55
Cameron LaVallie <camlavallie@gmail.com>
2019-03-28 13:27:24 -0500

    fix conflict

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import 'react-materialize';
 import './App.css';
 import Header from './Components/Header';
-<<<<<<< HEAD
 import Home from './Components/Pages/Home';
-=======
-import TextBody from './Components/TextBody';
-import Footer from './Components/Footer';
->>>>>>> f629f815bb0f9b013fb0bf50a9641f60f789a155
 
 class App extends Component {
   render() {
