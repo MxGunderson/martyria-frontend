f8f22f3b7a3f7a1345448743962cff01d9af52cd
Cameron LaVallie <camlavallie@gmail.com>
2019-04-10 08:11:45 -0500

    About page version 1.0

--- src/Components/Pages/About.js
+++ src/Components/Pages/About.js
@@ -2,9 +2,24 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-       <h1>About Martyria</h1>
-        <p>Martyria is.. Lorem ipsum dolor sit amet consectetur adipisicing elit. At eos optio sint neque corporis nam, saepe magni ratione repellat adipisci.</p>
-    </div>
+      <div className="landing">
+        <div className="dark-overlay landing-inner text-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <h3 className="display-3 mb-4">About Martyria - μαρτυρία</h3>
+                <p className="lead"> Pronunciation: mar - too - ree '-ah</p> <hr/>
+                <p className="lead">
+                  {' '}
+                  Record, Report, Testimony, Witness!
+                </p>
+                <hr />
+                 <p>Version 1.0</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  
   )
 }
