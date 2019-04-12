25e3aba7d74a50d8da76aed92bfc94201573d193
matthew.gunderson12@gmail.com <matthew.gunderson12@gmail.com>
2019-04-11 22:11:51 -0700

    navBar should change depending on user logged in or not, has not been tested

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import store from './store';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
-import { setCurrentUser } from './actions/authActions';
+import { setCurrentUser, logoutUser } from './actions/authActions';
 
 import Header from './Components/Layout/Header';
 import Footer from './Components/Layout/Footer';
@@ -18,36 +18,47 @@ import 'react-materialize';
 import './App.css';
 
 // Check for token
-if(localStorage.jwt_decode) {
+if (localStorage.jwt_decode) {
   // set auth token header auth
   setAuthToken(localStorage.jwtToken);
   //decode token and get user info and exp
   const decoded = jwt_decode(localStorage.jwtToken);
   // set user and isAuthenticated
   store.dispatch(setCurrentUser(decoded));
+
+  //check for expired token - which is set on the backend - check to see if that has been implemented
+  const currentTime = Date.now() / 1000
+  if (decoded.exp < currentTime) {
+    //logout user
+    store.dispatch(logoutUser)
+    //TODO: Clear current profile
+
+    //Redirect to login
+    window.location.href = '/login';
+  }
 }
 
 class App extends Component {
   render() {
     return (
-      <Provider store={ store }>
-      <Router>
-        <div className="App">
-          <Header className="header"/>
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path='/about' component={About} /> 
-              <Route path='/home' component={Home} />
-              <Route path='/contact' component={Contact} />
-              <Route path='/login' component={Login} />
-              <Route path='/signup' component={Signup} />
-              <Route path='/user' component={User} />
-            </Switch>
-          <Footer className="footer" />
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Header className="header" />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path='/about' component={About} />
+                <Route path='/home' component={Home} />
+                <Route path='/contact' component={Contact} />
+                <Route path='/login' component={Login} />
+                <Route path='/signup' component={Signup} />
+                <Route path='/user' component={User} />
+              </Switch>
+              <Footer className="footer" />
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
       </Provider>
     );
   }
