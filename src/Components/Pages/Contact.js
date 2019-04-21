3b9b4bf79ab2fe09e443df9a4b7447ea171226ba
Morgan <mmuse191@gmail.com>
2019-04-20 23:04:56 -0500

    changes made to contact page

--- src/Components/Pages/Contact.js
+++ src/Components/Pages/Contact.js
@@ -13,42 +13,51 @@ class Contact extends Component {
                   <p className="lead"> We care about your experience!</p> <hr />
                   <p className="lead"> Record, Report, Testimony, Witness!</p>
                   <hr />
-                  <p>Meet the developers: </p>
+                  <h3>Meet the developers: </h3>
                   <div class="grid-container">
                     <div class="col-md-6">
                       <h3>Cameron LaVallie</h3>
-                      < img className = "Cam" src = {require('../Img/cam.jpg')} alt="Cam"/>
-                      <p className="dev-info">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit...
-                      </p>
+                      <img
+                        className="Cam"
+                        src={require("../Img/cam.jpg")}
+                        alt="Cam"
+                      />
 
-                      <p />
+                      {/* <form
+                        action="https://www.facebook.com/cameron.lavallie"
+                        target="_blank"
+                      >
+                        <input
+                          type="submit"
+                          style={{ background: "#EFEDE1", color: "black" }}
+                          value="Follow me"
+                        />
+                      </form> */}
                     </div>
                     <div class="col-md-6">
                       <h3>Matthew Gunderson</h3>
-                      < img className = "Matt" src = {require('../Img/matt.jpg')} alt="Matt"/>
-                      <p className="dev-info">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit...
-                      </p>
+                      <img
+                        className="Matt"
+                        src={require("../Img/matt.jpg")}
+                        alt="Matt"
+                      />
                     </div>
-                    <br />
+
                     <div class="col-md-6">
                       <h3>Morgan Muse</h3>
-                      < img className = "Mo" src = {require('../Img/mo.jpg')} alt="Mo"/>
-                      <p className="dev-info">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit...
-                      </p>
+                      <img
+                        className="Mo"
+                        src={require("../Img/mo.jpg")}
+                        alt="Mo"
+                      />
                     </div>
                     <div class="col-md-6">
                       <h3>Paul Rase</h3>
-                      < img className = "Paul" src = {require('../Img/paul.jpg')} alt="Paul"/>
-                      <p className="dev-info"> 
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit...
-                      </p>
+                      <img
+                        className="Paul"
+                        src={require("../Img/paul.jpg")}
+                        alt="Paul"
+                      />
                     </div>
                   </div>
                 </div>
